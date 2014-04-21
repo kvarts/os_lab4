666863d1160258a26cf005a1b2642beb79b2b3b8
Ivan(PC) <mr.kochergin.ivan@gmail.com>
2014-04-21 13:19:21 +0400

    Второй коммит. Работает только при создании новой гаммы

--- main.c
+++ main.c
@@ -5,15 +5,17 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#include <errno.h>
+
 #define SIZE_BUF 4096
 
 #define EQU 1
 #define NO_EQU 0
-int f_in,f_out,f_gamma;
-
+int f_in, f_out, f_gamma;
+char n_in[255], n_gamma[255], n_out[255];
 int main(int argc, char *argv[])
 {
-	char *n_in,*n_gamma,*n_out;
+	
 	int opt;
 	
 	if(argc!=2 && argc !=3){
@@ -21,16 +23,14 @@ int main(int argc, char *argv[])
 		_exit(-1);
 	}
 	
-	n_gamma = NULL;
-	n_in = NULL;
 	/* Declare options */
 	while ((opt = getopt(argc, argv, ":i:g:")) != -1) {
 		switch (opt) {
 		case 'i':
-				n_in = optarg;
+				strcpy(n_in, optarg);
 				break;
 		case 'g':
-				n_gamma = optarg;
+				strcpy(n_gamma, optarg);
 				break;
 		case ':':
 				printf("Option need a value!\n");
@@ -40,8 +40,12 @@ int main(int argc, char *argv[])
 				_exit(-1);
 	   }
 	}
-	
-	if(!n_in){
+	/*
+	strcpy(n_in, argv[1]);
+	if(argc == 3)
+		strcpy(n_gamma, argv[2]);
+	*/
+	if(!strlen(n_in)){
 		printf("Error! Don`t have input file\n");//grammar!!!!!
 		_exit(-1);
 	}
@@ -52,9 +56,10 @@ int main(int argc, char *argv[])
 		_exit(-1);
 	}
 	
-	if(!n_gamma){
-		n_gamma = strcat(n_in, ".gamma");
-		f_gamma = open(n_gamma,  O_RDWR|O_CREAT, 0777);
+	if(!strlen(n_gamma)){
+		strcpy(n_gamma, n_in);
+		strcat(n_gamma, ".gamma");
+		f_gamma = open(n_gamma, O_CREAT, 0777);
 		if (!f_gamma) {
 			printf("error open\n");
 			_exit(-1);
@@ -69,9 +74,9 @@ int main(int argc, char *argv[])
 		if (check_gamma() == NO_EQU)
 			create_gamma();
 	}
-	
-	n_out = strcat(n_in, ".result");
-	f_out = open(n_out, O_WRONLY|O_CREAT, 0777);
+	strcpy(n_out, n_in);
+	strcat(n_out, ".result");
+	f_out = open(n_out, O_CREAT, 0777);
 	if (!f_out) {
 		printf("error open\n");
 		_exit(-1);
@@ -88,32 +93,35 @@ int create_gamma()
 {
 	char buf[SIZE_BUF];
 	unsigned int size_gamma, i, j;
-	struct stat *s_in;
-	if (fstat(f_in, s_in) == -1){
+	struct stat s_in;
+	if (stat(n_in, &s_in) == -1){
+		printf("name = %s\n", n_in);
 		printf("Error! Fstat fuction return error value!\n");
+		printf("Comment: %s\n",strerror(errno));
 		return -1;
 	}
-	printf("\nA\n");
-	size_gamma = s_in->st_size;
-	for(i = 1; i <= size_gamma; i++) {
+	size_gamma = s_in.st_size;
+	for(i = 1; i <= size_gamma;) {
 		for(j = 1; j <= SIZE_BUF && i <= size_gamma; j++, i++)
 			buf[j-1] = rand() % 256;
-		write (f_gamma, buf, j);
+		write (f_gamma, buf, j-1);
 	}
 }
 
 int check_gamma()
 {
-	struct stat *s_in, *s_gamma;
-	if (fstat(f_in, s_in) == -1){
+	struct stat s_in, s_gamma;
+	if (stat(n_in, &s_in) == -1){
 		printf("Error! Fstat fuction return error value!\n");
+		printf("Comment: %s\n",strerror(errno));	
 		_exit(-1);
 	}
-	if (fstat(f_gamma, s_gamma) == -1){
+	if (stat(n_gamma, &s_gamma) == -1){
 		printf("Error! Fstat fuction return error value!\n");
+		printf("Comment: %s\n",strerror(errno));
 		_exit(-1);
 	}
-	if (s_in->st_size != s_gamma->st_size)
+	if (s_in.st_size != s_gamma.st_size)
 		return NO_EQU;
 	else return EQU;
 }
@@ -121,17 +129,21 @@ int check_gamma()
 int gamming()
 {
 	char buf_in[SIZE_BUF], buf_gamma[SIZE_BUF], buf_out[SIZE_BUF];
-	unsigned int j, cnt_read;
+	unsigned int i, cnt_read;
 	cnt_read = SIZE_BUF;
 	while(cnt_read == SIZE_BUF) {
 		cnt_read = 	read(f_in, buf_in, SIZE_BUF);
 					read(f_gamma, buf_gamma, SIZE_BUF);
 		if (!cnt_read)
 			break;
-		for(j = 1; j <= cnt_read; j++) {
-			buf_out[j-1] = buf_in[j-1] ^ buf_gamma[j-1];
+		printf("in:\n\t%s\n", buf_in);
+		printf("gamma:\n\t%s\n", buf_gamma);
+		
+		for(i = 0; i < cnt_read; i++) {
+			buf_out[i] = buf_in[i] ^ buf_gamma[i];
 		}
-		write (f_gamma, buf_gamma, cnt_read);
+		printf("out:\n\t%s\n", buf_out);
+		write (f_out, buf_out, cnt_read);
 	}
 	return 0;
 }
