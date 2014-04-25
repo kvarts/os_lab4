9690424b1fed72e21036d1046e4c4c61bf6d7e5c
Kochergin Ivan <mr.kochergin.ivan@gmail.com>
2014-04-25 19:01:25 +0400

    Переделана логика. Добавлены пайпы. Добавлен файл, содержащий статическую гамму

--- main.c
+++ main.c
@@ -1,108 +1,100 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
+#include <wait.h>
 #include <sys/stat.h>
-
+#include <signal.h>
 #include <errno.h>
 
 #define SIZE_BUF 4096
+int flag_end = 0;
+void handler(int sig) {
+	wait(NULL);
+	flag_end = flag_end + 1;
+}
 
-#define EQU 1
-#define NO_EQU 0
-int f_in, f_out, f_gamma;
-char n_in[255], n_gamma[255], n_out[255];
 int main(int argc, char *argv[])
 {
-
-	int opt;
-
-	if(argc!=2 && argc !=3){
-		printf("Error! Count of argument is not 1 or 2\n");
+	(void)signal(SIGCHLD, handler);
+	int f_in, f_out, f_gamma;
+	char n_out[255];
+	int pipe_in[2], pipe_gamma[2];
+	char buffer[SIZE_BUF];
+	int cnt_read;
+	if(argc !=3){
+		printf("Error! Count of argument is not 2\n");
 		_exit(-1);
 	}
 
-	strcpy(n_in, argv[1]);
-	if(!(f_in = open(n_in, O_RDONLY)))
+	if(pipe(pipe_in) == -1)
 		error_exit(errno);
-		
-	if (argc == 2) {
-		strcpy(n_gamma, n_in);
-		strcat(n_gamma, "_gamma");
-	}
-	else
-		strcpy(n_gamma, argv[2]);
-
-	if (!(f_gamma = open(n_gamma,  O_RDWR|O_CREAT, 0777)))
-		error_exit(errno);
-	if (check_gamma() == NO_EQU){
-		printf("create new gamma!\n");
-		create_gamma();
-	}
-
-	strcpy(n_out, n_in);
-	strcat(n_out, "_result");
-	if (!(f_out = open(n_out, O_RDWR|O_CREAT, 0777)))
-		error_exit(errno);
-
-	gamming();
-	close(f_in);
-	close(f_gamma);
-	close(f_out);
-	_exit(0);
-}
-
-int create_gamma()
-{
-	char buf[SIZE_BUF];
-	unsigned int size_gamma, j;
-	struct stat s_in;
-	if (stat(n_in, &s_in) == -1)
-		error_exit(errno);
-
-	size_gamma = s_in.st_size;
-	while(size_gamma) {
-		for(j = 0; j < SIZE_BUF && size_gamma; j++){
-			buf[j] = rand() % 256;
-			size_gamma--;
-		}
-		if (write(f_gamma, buf, j) == -1)
+	
+	pid_t pid_in = fork();
+	switch(pid_in) {
+		case -1:
 			error_exit(errno);
-	}
-	lseek(f_gamma, 0, SEEK_SET);
-	return 0;
-}
-
-int check_gamma()
-{ 
-	struct stat s_in, s_gamma;
-	if (stat(n_in, &s_in) == -1 || stat(n_gamma, &s_gamma) == -1) {
-		error_exit(errno);
-	}
-	if (s_in.st_size != s_gamma.st_size)
-		return NO_EQU;
-	else return EQU;
-}
-
-int gamming()
-{
-	char buf_in[SIZE_BUF], buf_gamma[SIZE_BUF], buf_out[SIZE_BUF];
-	unsigned int i, cnt_read;
-	cnt_read = SIZE_BUF;
-	while(cnt_read == SIZE_BUF) {
-		cnt_read = 	read(f_in, buf_in, SIZE_BUF);
-					read(f_gamma, buf_gamma, SIZE_BUF);
-		if (!cnt_read)
+		case 0:
+			close(pipe_in[0]);
+			f_in = open(argv[1], O_RDONLY);
+			if(!f_in)
+				error_exit(errno);
+			while((cnt_read = read(f_in, buffer, SIZE_BUF))!=0) {
+				write(pipe_in[1], buffer, cnt_read);
+			}
+			close(pipe_in[1]);
+			_exit(EXIT_SUCCESS);
+		case 1:
 			break;
-		printf("in:\t\t%s\n", buf_in);		
-		for(i = 0; i < cnt_read; i++) {
-			buf_out[i] = buf_in[i] ^ buf_gamma[i];
-		}
-		printf("out:\t\t%s\n", buf_out);
-		write (f_out, buf_out, cnt_read);
 	}
-	return 0;
+
+	if(pipe(pipe_gamma) == -1)
+		error_exit(errno);
+
+	pid_t pid_gamma = fork();
+	switch(pid_gamma) {
+		case -1:
+			error_exit(errno);
+		case 0:
+			close(pipe_gamma[0]);
+			f_gamma = open(argv[2], O_RDONLY);
+			if(!f_gamma)
+				error_exit(errno);
+			while((cnt_read = read(f_gamma, buffer, SIZE_BUF))!=0) {
+				write(pipe_gamma[1], buffer, cnt_read);
+			}
+			close(pipe_gamma[1]);
+			_exit(EXIT_SUCCESS);
+		case 1:
+			break;
+	}
+	close(pipe_in[1]);
+	close(pipe_gamma[1]);
+	char buf_in[SIZE_BUF],buf_gamma[SIZE_BUF], buf_out[SIZE_BUF];
+	strcpy(n_out, argv[1]);
+	strcat(n_out, "_out");
+	f_out = creat(n_out, 0777);
+	if(!f_out)
+		error_exit(errno);
+	int cnt_read_in, cnt_read_gamma;
+	int i;
+	while(1) {
+		cnt_read_in = read(pipe_in[0], buf_in, SIZE_BUF);
+		cnt_read_gamma = read(pipe_gamma[0], buf_gamma, SIZE_BUF);
+		/*if(cnt_read_in != cnt_read_gamma){
+			printf("read cnts isn`t equ!\n");
+			_exit(EXIT_FAILURE);
+		}*/
+		for(i = 0; i<cnt_read_in; i++)
+			buf_out[i] = buf_in[i] ^ buf_gamma[i];			
+		write(f_out, buf_out, cnt_read_in);
+		if (flag_end == 2)
+			break;
+	}
+	close(f_out);
+	_exit(EXIT_SUCCESS);
 }
 
 int error_exit(int id_error)
@@ -110,4 +102,3 @@ int error_exit(int id_error)
 	printf("Error! Comment: %s\n",strerror(errno));
 	_exit(-1);
 }
-
